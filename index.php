649cfa205ac4959b3c13d6abb1fd1cbf5f12fc03
Rehan Dev <125759112+Publicpvdev@users.noreply.github.com>
2023-02-19 00:48:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Php</title>
+  </head>
+  <body>
+    
+    <?php
+    echo "Halo Ini Diketik Menggunakan Bahasa Php Dan Bahasa Indonesia";
+    ?>
+  </body>
+</html>
